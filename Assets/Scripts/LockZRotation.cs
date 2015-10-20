3a8316d73804f8b53215885cd6841e4090595a39
Victor Nogueira <felladrin@gmail.com>
2015-10-19 23:34:30 -0300

    first commit

---
+++ Assets/Scripts/LockZRotation.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LockZRotation : MonoBehaviour {
+	void Update () {
+        transform.rotation = Quaternion.AngleAxis(0, Vector3.forward);
+    }
+}
